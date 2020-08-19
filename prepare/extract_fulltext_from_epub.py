9d4451d1dd037d1799747a63b47610b9963ed31d
jinpeng <jinpeng.dong@gmail.com>
2020-08-19 10:18:10 +0800

    Split chapter caption into three segements.

--- prepare/extract_fulltext_from_epub.py
+++ prepare/extract_fulltext_from_epub.py
@@ -10,14 +10,25 @@ def epub2thtml(epub_path):
             chapters.append(item.get_content())
     return chapters
 
+# <h1 class="chapterCaption"><a id="CHP14"></a>第十三回<br/>李傕郭汜大交兵<br/>杨奉董承双救驾</h1>
+# <h1 class="chapterCaption1"><a id="CHP19"></a>第十八回<br/>贾文和料敌决胜<br/>夏侯惇拔矢啖睛<sup><a class="duokan-footnote" href="#jz_1_172" id="jzyy_1_172"><img alt="" src="../Images/note.png"/></a></sup></h1>
+def split_chapter_caption(h1_element):
+    output = []
+    for child in h1_element.children:
+        if child.string != None:
+            # print(child.string)
+            output.append(child.string)
+    return '，'.join(output) + "。"
+
 def thtml2text(thtml):
     # print(thtml.decode('utf-8'))
     output = []
     soup = BeautifulSoup(thtml.decode('utf-8'), 'html.parser')
     captions = soup.find_all('h1')
     for caption in captions:
-        # print(caption.text)
-        output.append(caption.text)
+        splitted_caption  = split_chapter_caption(caption)
+        # print(splitted_caption)
+        output.append(splitted_caption)
     paragraphs = soup.find_all("p", class_="bodyContent")
     for paragraph in paragraphs:
         # print(paragraph.text)
@@ -25,9 +36,7 @@ def thtml2text(thtml):
     return '\n'.join(output)
   
 '''
-    TODO: 1. Fix some chapter caption loss.
-          2. Split chapter caption into 3 segements.
-          3. Convert rare characters from images to text.
+    TODO: Convert rare characters from images to text.
 '''
 if __name__ == '__main__':
     chapters = epub2thtml("../data/raw/sgyy.epub")
